4a221b61d9fea0e3bc7e26a6c81252f01bb03128
devs-rakib <rakibjp431@gmail.com>
2025-10-01 22:10:46 +0600

    While Loop

---
+++ app.js
@@ -0,0 +1,16 @@
+// for (i = 0; i <= 100; i++) {
+//     console.log("Hello World" +" " + i)
+// }
+
+
+let age = Number(prompt("Enter your age"));
+ while (age < 18) {
+    console.log("Your are yeang")
+    Number(prompt("Your are yeang, Enter your age again"));
+    age++;
+
+ }
+
+ if (age >= 18) {
+    prompt("You can drive");
+ }
