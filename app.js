9e51b29a2fba655a36050eb9315d355b3cd3b698
devs-rakib <rakibjp431@gmail.com>
2025-10-08 16:17:02 +0600

    Factorial Faction

--- app.js
+++ app.js
@@ -3,14 +3,35 @@
 // }
 
 
-let age = Number(prompt("Enter your age"));
- while (age < 18) {
-    console.log("Your are yeang")
-    Number(prompt("Your are yeang, Enter your age again"));
-    age++;
+// let age = Number(prompt("Enter your age"));
+//  while (age < 18) {
+//     console.log("Your are yeang")
+//     Number(prompt("Your are yeang, Enter your age again"));
+//     age++;
 
- }
+//  }
 
- if (age >= 18) {
-    prompt("You can drive");
- }
+//  if (age >= 18) {
+//     prompt("You can drive");
+//  }
+
+// console.log("Hello World");
+
+// const arr = [1, 2, 3, 4, 5];
+
+// console.log(arr[3]);
+
+const person = (name, age) => {
+   return {
+      name: name,
+      age: age,
+       sayHello() {
+         console.log("Hello " + this.name);
+      }
+   }
+}
+
+const person1 = person("John", 25);
+const person2 = person("Jane", 30);
+console.log(person1); // John
+console.log(person2.sayHello());
